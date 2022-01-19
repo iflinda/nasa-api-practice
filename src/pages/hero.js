9b0461f15d652cf4921d38720e72b32a69a62f70
iflinda <informlinda@gmail.com>
2022-01-18 21:34:36 -0800

    Styled and added links for main section of the website

--- src/pages/hero.js
+++ src/pages/hero.js
@@ -1,10 +1,35 @@
 import React from "react";
-import astronaut from "../images/astronaut.svg";
+import { Link } from "react-router-dom";
+import Astronaut from "../images/astronaut.svg";
+import Logo from "../images/logo.svg";
 
-export default function Hero() {
+export default function hero() {
   return (
-    <main className="flex flex-col">
-      <img src={astronaut} className="h-screen" alt="" />
+    <main
+      id="hero"
+      className="fixed h-screen flex flex-col z-background text-center"
+    >
+      <img
+        src={Astronaut}
+        className="h-5/6 z-background"
+        alt="An astronaut standing on a reddish purple planet that is upside-down in space."
+      />
+      <img
+        src={Logo}
+        className="h-logoHeroHeight sm:mt-marginHeroLogo mobile:mt-marginHeroLogoMobile z-1"
+        alt="Spaceroid Logo"
+      />
+      <h1 className="m-3">
+        The finest collection of space-related images on the internet.
+      </h1>
+      <Link to="/images">
+        <li
+          key="images"
+          className="button inline p-1 px-5 bg-shootingStar rounded-lg"
+        >
+          Start Browsing
+        </li>
+      </Link>
     </main>
   );
 }
