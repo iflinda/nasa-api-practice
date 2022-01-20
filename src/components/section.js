18ad35281746ee135823e86b07c4ff75229b99b4
iflinda <informlinda@gmail.com>
2022-01-19 19:31:12 -0800

    Completed tasks and tried to bug fix loading bar.

---
+++ src/components/section.js
@@ -0,0 +1,123 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/no-unused-state */
+import React, { Component } from "react";
+import axios from "axios";
+import LazyLoad from "react-lazyload";
+import "../styles/global.css";
+import errorImage from "../images/404.png";
+
+const apiKey = process.env.REACT_APP_NASA_API_KEY;
+
+function Loading() {
+  return (
+    <div className="w-screen border-8 border-shootingStar border-dashed rounded-full animate-slow" />
+  );
+}
+
+function changeButton(id) {
+  const el = document.getElementById(id);
+  if (el.value === "Like") {
+    el.value = "Unlike";
+    el.style.backgroundColor = "#7A7381";
+    el.innerHTML = "Unlike";
+  } else if (el.value === "Unlike") {
+    el.value = "Like";
+    el.innerHTML = "Like";
+    el.style.backgroundColor = "#9E1A21";
+  }
+}
+
+export default class Section extends Component {
+  constructor() {
+    super();
+    this.state = {
+      photos: null,
+      isLoaded: false,
+      hasError: false,
+      errorMessage: "",
+    };
+  }
+
+  async componentDidMount() {
+    axios
+      .get(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=100`)
+      .then((res) => {
+        const { data } = res;
+        for (let i = 0; i <= data.length - 1; i += 1) {
+          data[i].liked = false;
+          data[i].index = i;
+          this.setState({ photos: data });
+        }
+        this.setState({ isLoaded: true });
+      })
+      .catch((err) => {
+        this.setState({ hasError: true });
+        this.setState({ errorMessage: err });
+      });
+  }
+
+  likePhoto(index) {
+    const favourites = [...this.state.photos];
+    favourites[index].liked = !favourites[index].liked;
+  }
+
+  loadPage() {
+    if (!this.state.isLoaded) {
+      return (
+        <div className="w-screen border-8 border-shootingStar border-dashed rounded-full animate-slow" />
+      );
+    }
+    return (
+      <div className="flex flex-row flex-wrap justify-center">
+        {this.state.photos.map((results) => {
+          return (
+            <div key={results.url}>
+              <LazyLoad placeholder={<Loading />}>
+                <article className="w-fit bg-white p-6 m-6 rounded-lg">
+                  <h2 className="my-3">{results.title}</h2>
+                  <div>
+                    <img
+                      src={results.url}
+                      className="mobile:w-imageWidthMobile mobile:h-imageHeightMobile sm:w-imageWidth sm:h-imageHeight object-cover rounded-lg"
+                      onError={(event) => {
+                        event.target.src = `${errorImage}`;
+                      }}
+                      alt={results.title}
+                    />
+                  </div>
+                  <div className="flex justify-between mt-4">
+                    <div>{results.date}</div>
+                    <button
+                      id={results.index}
+                      className="likeButton bg-fallingStar px-5 rounded-lg w-buttonWidth min-w-[86.08px]"
+                      type="submit"
+                      value="Like"
+                      onClick={() => {
+                        this.likePhoto(results.index);
+                        changeButton(results.index);
+                      }}
+                    >
+                      Like
+                    </button>
+                  </div>
+                </article>
+              </LazyLoad>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+
+  render() {
+    if (this.state.hasError) {
+      <h1>Oh no! Something has went wrong. 😢</h1>;
+    }
+    return (
+      <section>
+        <h1 className="text-center m-6">Astronomy Pictures of the Day</h1>
+        {this.loadPage()}
+      </section>
+    );
+  }
+}
--- src/pages/hero.js
+++ src/pages/hero.js
@@ -3,33 +3,30 @@ import { Link } from "react-router-dom";
 import Astronaut from "../images/astronaut.svg";
 import Logo from "../images/logo.svg";
 
-export default function hero() {
+export default function Hero() {
   return (
-    <main
-      id="hero"
-      className="fixed h-screen flex flex-col z-background text-center"
-    >
+    <main className="flex flex-col justify-center text-center z-background">
       <img
+        id="hero"
         src={Astronaut}
-        className="h-5/6 z-background"
+        className="h-[calc(100vh-30px)] rotate-180"
         alt="An astronaut standing on a reddish purple planet that is upside-down in space."
       />
-      <img
-        src={Logo}
-        className="h-logoHeroHeight sm:mt-marginHeroLogo mobile:mt-marginHeroLogoMobile z-1"
-        alt="Spaceroid Logo"
-      />
-      <h1 className="m-3">
-        The finest collection of space-related images on the internet.
-      </h1>
-      <Link to="/images">
-        <li
-          key="images"
-          className="button inline p-3 px-5 bg-shootingStar rounded-lg"
-        >
-          Start Browsing
-        </li>
-      </Link>
+      <div className="tagline w-full fixed flex flex-col z-10">
+        <img src={Logo} className="h-logoHeroHeight" alt="Spaceroid Logo" />
+        <h1 className="m-3">
+          The finest collection of space-related images on the internet.
+        </h1>
+        <Link to="/main">
+          <button
+            key="images"
+            type="button"
+            className="button inline p-3 px-5 bg-shootingStar rounded-lg"
+          >
+            Start Browsing
+          </button>
+        </Link>
+      </div>
     </main>
   );
 }
