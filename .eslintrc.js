ac2058da74cb16bcb586eb56675c5bf47e95c078
iflinda <informlinda@gmail.com>
2022-03-05 20:28:09 -0800

    Fixed minor styling issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,31 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    "airbnb",
-    "plugin:react/recommended",
-    "plugin:prettier/recommended",
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        browser: true,
+        es2021: true,
     },
-    ecmaVersion: 12,
-    sourceType: "module",
-  },
-  plugins: ["react"],
-  rules: {
-    "react/react-in-jsx-scope": "off",
-    "react/jsx-filename-extension": [0],
-    "no-param-reassign": [2, { props: false }],
-    "import/extensions": "off",
-    "prettier/prettier": [
-      "warn",
-      {
-        endOfLine: "auto",
-      },
+    extends: [
+        "airbnb",
+        "plugin:react/recommended",
+        "plugin:prettier/recommended",
     ],
-  },
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: "module",
+    },
+    plugins: ["react"],
+    rules: {
+        "react/react-in-jsx-scope": "off",
+        "react/jsx-filename-extension": [0],
+        "no-param-reassign": [2, { props: false }],
+        "import/extensions": "off",
+        "prettier/prettier": [
+            "warn",
+            {
+                endOfLine: "auto",
+            },
+        ],
+    },
 };
